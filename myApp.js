09075e0cff9a462ba32758178c171dd0a1b78937
Coatzel <coatzel21@gmail.com>
2016-12-30 19:30:57 -0500

    Add files via upload
    Added Version Control

--- myApp.js
+++ myApp.js
@@ -1,13 +1,37 @@
 
 
 
-		var app = angular.module("myApp", []);
-		app.controller("myCtrl", function($scope) {
+		var app = angular.module("myApp", []).constant('VERSION', 1.1)
+		app.controller("myCtrl", function(VERSION,$scope) {
+
+			this.version=VERSION;
+
+
+
+			console.log($scope);
 		  $scope.myTxt = "You have not yet clicked submit";
-		  $scope.myFunc = function () {
+
+			$scope.myFunc = function () {
 		      $scope.myTxt = "You clicked submit!";
+					}
 
-    			console.log($scope);
 
-				}
-						  });
+
+		  $scope.clearForm = function (){
+					$scope.sound =null;
+					$scope.verbing=null;
+					$scope.oof=null;
+					$scope.body=null;
+					$scope.adj=null;
+					$scope.name=null;
+					$scope.number=null;
+					$scope.fruit=null;
+					$scope.rhyme1=null;
+					$scope.rhyme2=null;
+					$scope.organ=null;
+					$scope.myth=null;
+					$scope.doom=null;
+
+			};
+
+			  });
